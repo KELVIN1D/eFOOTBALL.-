9ef062fa0c1d3d1b6a5d4841fd25d8bc2506963d
KELVIN1D <147503856+KELVIN1D@users.noreply.github.com>
2024-10-28 10:24:17 -0300

    ,

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+function criaCartao(pergunta, resposta){
+    let conteudo = document.getElementById('conteudo')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao--conteudo">
+    <div class="cartao--pergunta"><p>${pergunta}</p><
+    <div class="cartao--resposta"><p>${resposta}</p><
+    </div>
+    
+    `
+    console.log(cartao)
+    conteudo.appendChild(cartao)
+
+    
+
+
+
+
+
+}
