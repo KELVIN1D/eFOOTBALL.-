68d568ffb4cec56df0a5d51e620f51a27fc5bb00
darkspirit1920k <147504011+darkspirit1920k@users.noreply.github.com>
2024-10-29 08:45:33 -0300

    e

--- assets/pergunta.js
+++ assets/pergunta.js
@@ -1,25 +1,25 @@
 criaCartao(
-    "Como driblar"
+    "Como driblar",
     "De um clique no acelerar e uma arrastada no analógico rapidamente"
 )
 criaCartao{
-    "Como chutar colocado"
+    "Como chutar colocado",
     "Arraste o botão de chute para baixo"
 }
 criaCartao( 
-    "Como fazer super chut"
+    "Como fazer super chut",
     "Arraste o botão de chute para esquerda ou direita"
 )
 criaCartao( 
-    "Quem é o melhor jogador"
+    "Quem é o melhor jogador",
     "Neymar"
 )
 criaCartao(
-    "Como bater escanteio"
+    "Como bater escanteio",
     "Deslize o dedo na tela para onde quer que a bola vá "
 )
 criaCartao( 
 
-    "Como bater falta"
+    "Como bater falta",
     "Deslize o dedo em direção ao gol"
 )
